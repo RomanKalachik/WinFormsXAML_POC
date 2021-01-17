41aca46f1ef07bc3fd77b4b6680f798bbd6a96da
Roman Kalachik <RomanKalachik@gmail.com>
2021-01-17 18:04:09 +0300

    first commit

---
+++ WindowsFormsApp3/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WpfApp2 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Form {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, EventArgs e) {
+            MessageBox.Show("Button click!");
+        }
+    }
+}
